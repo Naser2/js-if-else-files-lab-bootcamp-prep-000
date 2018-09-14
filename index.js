ad0a0ce8e5b02d8b49bbf8d53af2bb9ff507823d
Nasser Sanou <nassersanou23@gmail.com>
2018-09-14 22:36:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if ( name === "Susane") {
   console.log("The name is correct")
 } else {
   console.log (`expected the name yo equal ${name}`)
-} ;
+} 
 if ( height === "70") {
   console.log("The height is correct")
 } else {
